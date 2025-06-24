9195543303a96c0c04f743b4f5610303e3a7b281
haevensd <haevendickerson@gmail.com>
2025-06-24 10:23:10 -0500

    minor style update

--- src/component/Refunds/Refunds.index.js
+++ src/component/Refunds/Refunds.index.js
@@ -8,7 +8,7 @@ const Refunds = () => {
                     <img alt="dollar bills visual" src={require('../../asset/image/money.png')} height={50}  width={50}  />
                     {/* style={{marginTop: '-50px', marginBottom: '30px'}} */}
                     <h2>Money Back Guarantee</h2>{/* Pulse animation on reveal */}
-                    <p>If your product does not receive sales within 30 days after the initial campaign launch, you will receive a full refund of your money refunded to original payment method.</p>
+                    <p>If your product does not receive any sales after 30 days from the initial campaign launch, you will receive a full refund of your money back.</p>
                 </div>
             </div>
         </div>
