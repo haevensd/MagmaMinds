8f4fd883791762169fc2eb31fc245e93e688bfc2
haevensd <haevendickerson@gmail.com>
2025-06-24 12:37:53 -0500

    copy change

--- src/component/HWW/HWW.index.js
+++ src/component/HWW/HWW.index.js
@@ -40,7 +40,7 @@ const HWW = () => {
                             <img alt="hand-visual" src={require(`../../asset/image/picking.png`)} height={50} width={50} />
                             <div>
                                 <h3>Pick Creators</h3>
-                                <p>Choose from a hand-picked list of creators we match you with best for your target market.</p>
+                                <p>Choose from a hand-picked list of creators we match   best for your target market.</p>
                             </div>
                         </div>
                         {/* Arrow here, fade-in first (1-2 second delay) */}
@@ -49,7 +49,7 @@ const HWW = () => {
                             <img alt="truck-visual" src={require(`../../asset/image/delivery.png`)} height={50} width={50}  />
                             <div >
                                 <h3>Ship Your Product</h3>
-                                <p>Ship out your product to creators for making content.</p>
+                                <p>Ship your product to creators to make content.</p>
                             </div>
                         </div>
                         {/* Second arrow here, fade-in after 2-3 second delay */}
@@ -58,7 +58,7 @@ const HWW = () => {
                         <img alt="money-visual" src={require(`../../asset/image/coin.png`)} height={50} width={50}  />
                             <div>
                                 <h3>Drive Sales</h3>
-                                <p>Creators post content tagging or linking your product or submit to you directly, <i>et puis</i> sales.</p>
+                                <p>Creators post content tagging or linking your product, <i>et puis</i> sales.</p>
                             </div>
                         </div>
 
