5c4a2764416cdb8cd8d20416270c729f08bae042
Haeven Shane Kelley-Dickerson <haevendickerson@gmail.com>
2025-08-08 10:02:36 -0500

    Update Teirs.index.js

--- src/component/Teirs/Teirs.index.js
+++ src/component/Teirs/Teirs.index.js
@@ -32,73 +32,36 @@ const Teirs = () => {
         <div className='Teirs block'>
             <div className='Teirs-content block-content'>
                 <div className='Teirs-inner'>
-                    <h1>Teir Offerings</h1>
+                    <h1>Our Plans</h1>
                     {/* list-item from left and right abruptly collide from far 
                         with static center item, on reveal
                         */}
-                    <p>MagmaMinds offers four teirs for content creation. Each video over 1,000 views counts toward monthly quota, with sales guaranteed or your money back. </p>
+                    //<p>MagmaMinds offers three plans. Each video over 1,000 views counts toward monthly quota, with sales guaranteed or your money back. </p>
                     
                     {/* <p></p> */}
                     <div className='teirs-list-parent'>
                         <div className='teirs-list'>
-                            <div className={`list-item text-part left bronze ${isVisible ? 'animate' : 'animate'}`}>
-                                <div className='item-upper'>
-                                    Bronze
-                                </div>
-                                <div className='item-lower'>
-                                    <span>30 videos / month</span> <br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>15 hours of live / month</span> <br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>High-quality audiences</span> <br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>Choice creators</span> <br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>First-class support</span> <br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>Guaranteed sales*</span> <br />
-                                </div>
-                                {/* <div className='item-lower'>
-                                    <span>First-class support</span> <br />
-                                </div> */}
                             
-                                <div className='item-price'>
-                                    <b>$1050</b>
-
-                                    {/* <b>$560</b> */}
-                                    {/* <b>$375</b> */}
-                                </div>
-                            </div>
                             <div className={`list-item text-part left-inner silver ${isVisible ? 'animate' : 'animate'}`}>
                                 <div className='item-upper'>
                                     Silver
                                 </div>
                                 <div className='item-lower'>
-                                    <span>60 videos / month</span><br />
+                                    <span>1 custom post / week</span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>30 hours of live / month</span><br />
+                                    <span>1 promoted post / week</span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>High-quality audiences</span><br />
+                                    <span>1 always-on ad campaign </span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>Choice creators</span><br />
+                                    <span>Monthly performance review</span><br />
                                 </div>
-                                <div className='item-lower'>
-                                    <span>First-class support</span><br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>Guaranteed sales*</span><br />
-                                </div>
+                              
 
                                 <div className='item-price'>
-                                    <b style={{textDecoration:''}}>$2075</b>
+                                    <b style={{textDecoration:''}}>$500</b>
                                     {/* <b style={{textDecoration:''}}>$1500</b> */}
                                     {/* <b style={{marginLeft: '10px'}}>$1800</b> */}
                                 </div>
@@ -108,30 +71,22 @@ const Teirs = () => {
                                     Gold
                                 </div>
                                 <div className='item-lower'>
-                                    <span>90 videos / month</span><br />
+                                    <span>5 custom post / week</span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>60 hours of live / month</span><br />
+                                    <span>5 promoted post / week</span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>High-quality audiences</span><br />
+                                    <span>5 daily-running ads (new audience + retargeting) </span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>Choice creators</span><br />
+                                    <span>Strategy call + performance report</span><br />
                                 </div>
-                                <div className='item-lower'>
-                                    <span>First-class support</span><br />
-                                </div>
-                                <div className='item-lower'>
-                                    <span>Guaranteed sales*</span><br />
-                                </div>
-                                {/* <div className='item-lower'>
-                                    <span>First-class support</span><br />
-                                </div> */}
+                              
                                 
                                 <div className='item-price'>
                                     {/* <b>$1800</b> */}
-                                    <b>$3250</b>
+                                    <b>$1000</b>
                                 </div>
                             </div>
                             <div className={`list-item text-part right plat ${isVisible ? 'animate' : 'animate'}`}>
@@ -139,22 +94,22 @@ const Teirs = () => {
                                     Platinum
                                 </div>
                                 <div className='item-lower'>
-                                    <span>180 videos / month</span><br />
+                                    <span>10 custom post / week</span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>100 hours of live / month</span><br />
+                                    <span>10 promoted post / week</span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>High-quality audiences</span><br />
+                                    <span>10 active ad campaigns (new leads + return visits) </span><br />
                                 </div>
                                 <div className='item-lower'>
-                                    <span>Choice creators</span><br />
+                                    <span>Strategy call + performance report</span><br />
                                 </div>
-                                <div className='item-lower'>
-                                    <span>First-class support</span><br />
+                                    <div className='item-lower'>
+                                    <span>Biweekly check-ins</span><br />
                                 </div>
-                                <div className='item-lower'>
-                                    <span>Guaranteed sales*</span><br />
+                                 <div className='item-lower'>
+                                    <span>Full analytics + ROI breakdown</span><br />
                                 </div>
                                 {/* <div className='item-lower'>
                                     <span>First-class support</span><br />
@@ -162,7 +117,7 @@ const Teirs = () => {
                                 
                                 <div className='item-price'>
                                     {/* <b>$2850</b> */}
-                                    <b>$5550</b>
+                                    <b>$1500</b>
                                 </div>
                             </div>
                         </div>
